06ea854d668a353b0962a861853a9948a6aac967
WalterHorst <whorst42@gmail.com>
2023-11-23 11:52:40 -0300

    Centrado de login

--- Client/src/components/Login/Login.jsx
+++ Client/src/components/Login/Login.jsx
@@ -1,61 +1,58 @@
-
-import React, { useState } from 'react';
-import './Login.css'; 
-import SignUp from '../SingUp/SingUp';
+import React, { useState } from "react";
+import "./Login.css";
+import SignUp from "../SingUp/SingUp";
 
 const Login = (props) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [isSignUp, setIsSignUp] = useState(false);
 
-  const handleLogin = async () => {
-
-  };
+  const handleLogin = async () => {};
 
   const handleToggle = () => {
     setIsSignUp(!isSignUp);
   };
 
   return (
-    <div className="login-container">
-      {isSignUp ? (
-        <SignUp />
-      ) : (
-        <>
-          <h2>Login</h2>
-          <form>
-            <label>
-              Username:
-              <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Password:
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </label>
-            <br />
-            <button type="button" onClick={handleLogin}>
-              Log In
-            </button>
-          </form>
-        </>
-      )}
-      <p>
-        {isSignUp
-          ? 'Already have an account? '
-          : "Don't have an account? "}
-        <button onClick={handleToggle}>
-          {isSignUp ? 'Log In' : 'Sign Up'}
-        </button>
-      </p>
+    <div className="padreLogin">
+      <div className="login-container">
+        {isSignUp ? (
+          <SignUp />
+        ) : (
+          <>
+            <h2>Login</h2>
+            <form>
+              <label>
+                Username:
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </label>
+              <br />
+              <label>
+                Password:
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+              <br />
+              <button type="button" onClick={handleLogin}>
+                Log In
+              </button>
+            </form>
+          </>
+        )}
+        <p>
+          {isSignUp ? "Already have an account? " : "Don't have an account? "}
+          <button onClick={handleToggle}>
+            {isSignUp ? "Log In" : "Sign Up"}
+          </button>
+        </p>
+      </div>
     </div>
   );
 };
