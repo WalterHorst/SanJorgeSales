5e937fec67e155d9b6e6353efce1726d7b045af0
WalterHorst <whorst42@gmail.com>
2023-11-23 10:42:48 -0300

    "Navbar va quedando"

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./views/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <>
-      <h1>San Jorge Sales</h1>
-    </>
+    <main>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </main>
   );
 }
 
--- Client/src/components/NavBar/NavBar.jsx
+++ Client/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,28 @@
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+import { FaUser } from "react-icons/fa";
+
 const NavBar = () => {
   return (
-    <div>
-      <h1>Este es el componente NavBar</h1>
+    <div className="navbar">
+      <div className="logo">
+        <NavLink to={"/"}>
+          <img
+            className="logo"
+            src="https://lavanderiasanjorge.com/tienda/wp-content/uploads/2020/07/logo-lavanderia-tintoreria-san-jorge.png"
+          ></img>
+        </NavLink>
+      </div>
+      {/* <div className="user-icon">
+        <FaUser />
+      </div> */}
+      <div className="login-button">
+        <NavLink to={"/login"}>
+          <button>Iniciar Sesión</button>
+        </NavLink>
+      </div>
     </div>
   );
 };
+
 export default NavBar;
