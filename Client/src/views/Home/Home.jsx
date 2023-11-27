601c21dd8d2420c9d2559792d7a4202628d3b754
leonardorodriguez22 <santiyvalenyleo@gmail.com>
2023-11-27 19:43:06 -0300

    fistPage

--- Client/src/views/Home/Home.jsx
+++ Client/src/views/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Home.css";
 import Carrusel from "../../components/Carrusel/Carrusel";
+import FirstPage from "../../components/firstPage/firstPage";
+
 
 const Home = () => {
   return (
     <div>
       <div className="home-container">
         <Carrusel>s</Carrusel>
+        <FirstPage></FirstPage>
       </div>
     </div>
   );
