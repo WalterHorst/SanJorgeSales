7cbfec37a7e51eb4fbf3392c4b5570c9f829862d
WalterHorst <whorst42@gmail.com>
2023-11-28 12:31:06 -0300

    NavBar

--- Client/src/components/NavBar/NavBar.jsx
+++ Client/src/components/NavBar/NavBar.jsx
@@ -1,83 +1,9 @@
-import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 import { FaUser } from "react-icons/fa";
 import logo from "../../assets/Imagenes/Logo.png";
 
 const NavBar = () => {
-  const categories = [
-    "CATEGORIAS",
-    "Comidas",
-    "Ropa",
-    "Repuestos",
-    "Bazar",
-    "Librería",
-    "Marroquinerías",
-    "Juguetes",
-    "Viveros",
-    "Joyería",
-    "Motos",
-    "Automotor",
-    "Mercería",
-    "Cotillón",
-    "Repostería",
-    "Heladerías",
-    "Postres",
-    "Almacén",
-    "Kiosko",
-    "Distribuidoras",
-  ];
-
-  const aderidosList = [
-    "ADERIDOS",
-    "ADN Indumentaria",
-    "Alas Ferreterías",
-    "Angeline Boutique",
-    "Baruk",
-    "Bartolome",
-    "Ben Bru",
-    "Bertola Repuestos",
-    "Biancotti Lonas",
-    "Bravin Ferretería",
-    "Brulee",
-    "Capitan Pizza",
-    "Casablanca",
-    "Creaciones Ferretería",
-    "Cuia Exquisiteces",
-    "Date El Gusto",
-    "El Mercadito Verdulería",
-    "Fast Food",
-    "Guapas",
-    "Helados Ray Ban",
-    "Helados Yampy",
-    "Igui",
-    "Kala",
-    "Kiosko Remo",
-    "La Eurlogia",
-    "La Ideal",
-    "Lola",
-    "Lomitos 2x1",
-    "Luscher Service Automotor",
-    "M&M",
-    "Masasseci Motos",
-    "Maytena",
-    "Maximo Deportes",
-    "Milanski Fabrica de Milanesas",
-    "Mini San Almacén",
-    "Morra Electricidad",
-    "Orko",
-    "Otto",
-    "Pamero",
-    "Pascual Rodados",
-    "Puesto Ruta 6 Verdulería",
-    "Punto Clave Gráfica",
-    "Ruda Macho",
-    "Tatoo House",
-    "Tienda Leon",
-    "Tienda Los Amigos",
-    "Zalocco Repuestos",
-  ];
-
   return (
     <div className="navbar">
       <div className="logo">
@@ -85,38 +11,12 @@ const NavBar = () => {
           <img className="logo" src={logo} alt="Logo"></img>
         </NavLink>
       </div>
-      <div>
-        <NavLink  className="About" to="/about">About</NavLink>
-      </div>
-      <div className="categories-selector">
-        <select>
-          {categories.map((category, index) => (
-            <option key={index} value={`/category/${category.toLowerCase()}`}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div className="aderidos-selector">
-        <select>
-          {aderidosList.map((aderido, index) => (
-            <option
-              key={index}
-              value={`/aderido/${aderido.toLowerCase().replace(/\s/g, "-")}`}
-            >
-              {aderido}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div className="login-button">
+      <div className="login-button-container">
         <NavLink to="/login">
-          <div className="user-icon">
-            <FaUser />
-          </div>
-          <button>Iniciar Sesión</button>
+          <button className="login-button">Iniciar Sesión</button>
+        </NavLink>
+        <NavLink>
+          <button className="login-button">Crear cuenta</button>
         </NavLink>
       </div>
     </div>
