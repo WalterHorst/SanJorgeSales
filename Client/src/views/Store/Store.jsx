606087091f4c1033611501616d083685459ffe4c
LeonardoRodriguez22 <112113959+LeonardoRodriguez22@users.noreply.github.com>
2023-12-07 19:40:37 -0300

    Newbranch123 (#18)
    * admin panel
    
    * asd
    
    * asd

--- Client/src/views/Store/Store.jsx
+++ Client/src/views/Store/Store.jsx
@@ -1,4 +1,83 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import "./Store.css";
+
+// const Store = () => {
+//   const [coverPhotoUrl, setCoverPhotoUrl] = useState("");
+//   const [profilePictureUrl, setProfilePictureUrl] = useState("");
+//   const [sellerName, setSellerName] = useState("");
+//   const [sellerLocation, setSellerLocation] = useState("");
+//   const [itemsForSale, setItemsForSale] = useState([]);
+
+
+//   const handleAddItem = (item) => {
+//     setItemsForSale([...itemsForSale, item]);
+//   };
+
+//   return (
+//     <div className="profile-container">
+//       <div
+//         className="cover-photo"
+//         style={{ backgroundImage: `url(${coverPhotoUrl})` }}
+//       ></div>
+//       <div className="profile-details">
+//         <div
+//           className="profile-picture"
+//           style={{ backgroundImage: `url(${profilePictureUrl})` }}
+//         ></div>
+//         <h2>{sellerName}</h2>
+//         <p>{sellerLocation}</p>
+//       </div>
+//       <div className="items-for-sale">
+//         <h3>Elementos en Venta</h3>
+//         <ul>
+//           {itemsForSale.map((item, index) => (
+//             <li key={index}>{item}</li>
+//           ))}
+//         </ul>
+//       </div>
+//       <div className="user-inputs">
+//         <input
+//           type="text"
+//           placeholder="URL de la portada"
+//           value={coverPhotoUrl}
+//           onChange={(e) => setCoverPhotoUrl(e.target.value)}
+//         />
+//         <input
+//           type="text"
+//           placeholder="URL de la foto de perfil"
+//           value={profilePictureUrl}
+//           onChange={(e) => setProfilePictureUrl(e.target.value)}
+//         />
+//         <input
+//           type="text"
+//           placeholder="Nombre del Vendedor"
+//           value={sellerName}
+//           onChange={(e) => setSellerName(e.target.value)}
+//         />
+//         <input
+//           type="text"
+//           placeholder="Ubicación del Vendedor"
+//           value={sellerLocation}
+//           onChange={(e) => setSellerLocation(e.target.value)}
+//         />
+//         <input
+//           type="text"
+//           placeholder="Agregar elemento en venta"
+//           onKeyPress={(e) => {
+//             if (e.key === "Enter") {
+//               handleAddItem(e.target.value);
+//               e.target.value = "";
+//             }
+//           }}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Store;
+
+import React, { useState } from "react";
 import "./Store.css";
 
 const Store = () => {
@@ -6,34 +85,34 @@ const Store = () => {
   const [profilePictureUrl, setProfilePictureUrl] = useState("");
   const [sellerName, setSellerName] = useState("");
   const [sellerLocation, setSellerLocation] = useState("");
-  const [itemsForSale, setItemsForSale] = useState([]);
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [productImage, setProductImage] = useState(""); // Nuevo estado para la URL de la imagen
+  const [productList, setProductList] = useState([]);
 
-  const handleAddItem = (item) => {
-    setItemsForSale([...itemsForSale, item]);
+  
+
+  const handleAddProduct = () => {
+    const newProduct = {
+      name: productName,
+      price: productPrice,
+      image: productImage, // Agregar la URL de la imagen al objeto del producto
+    };
+
+    setProductList([...productList, newProduct]);
+    setProductName("");
+    setProductPrice("");
+    setProductImage(""); // Limpiar la URL de la imagen después de agregar el producto
+  };
+
+  const handleBuyProduct = (product) => {
+    // Lógica para la compra del producto
+    console.log(`Comprando ${product.name} por ${product.price}`);
   };
 
   return (
     <div className="profile-container">
-      <div
-        className="cover-photo"
-        style={{ backgroundImage: `url(${coverPhotoUrl})` }}
-      ></div>
-      <div className="profile-details">
-        <div
-          className="profile-picture"
-          style={{ backgroundImage: `url(${profilePictureUrl})` }}
-        ></div>
-        <h2>{sellerName}</h2>
-        <p>{sellerLocation}</p>
-      </div>
-      <div className="items-for-sale">
-        <h3>Elementos en Venta</h3>
-        <ul>
-          {itemsForSale.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </div>
+      <h3>Datos de tu tienda</h3>
       <div className="user-inputs">
         <input
           type="text"
@@ -59,17 +138,72 @@ const Store = () => {
           value={sellerLocation}
           onChange={(e) => setSellerLocation(e.target.value)}
         />
+      </div>
+      <div
+        className="cover-photo"
+        style={{ backgroundImage: `url(${coverPhotoUrl})` }}
+      ></div>
+      <div className="profile-details">
+        <div
+          className="profile-picture"
+          style={{ backgroundImage: `url(${profilePictureUrl})` }}
+        ></div>
+        <h2>{sellerName}</h2>
+        <p>{sellerLocation}</p>
+      </div>
+      <div className="items-for-sale">
+        <h3>Productos en Venta</h3>
+        <ul>
+          {productList.map((product, index) => (
+            <li key={index} className="product-card">
+              <img
+                src={product.image} // Usar la URL de la imagen del producto
+                alt={product.name}
+                className="product-image"
+              />
+              <div className="product-details">
+                <h4>{product.name}</h4>
+                <p>${product.price}</p>
+              </div>
+              <div className="product-actions">
+                <button onClick={() => handleBuyProduct(product)}>
+                  Modificar
+                </button>
+                <button onClick={() => handleBuyProduct(product)}>
+                  Eliminar
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <h3>Nuevo Producto</h3>
+      <div className="user-inputs">
         <input
           type="text"
-          placeholder="Agregar elemento en venta"
-          onKeyPress={(e) => {
-            if (e.key === "Enter") {
-              handleAddItem(e.target.value);
-              e.target.value = "";
-            }
-          }}
+          placeholder="URL de la imagen del producto"
+          value={productImage}
+          onChange={(e) => setProductImage(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Nombre del Producto"
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Precio del Producto"
+          value={productPrice}
+          onChange={(e) => setProductPrice(e.target.value)}
         />
       </div>
+        <button className="agregar-button" onClick={handleAddProduct}>Agregar</button>
+    <div className="submit-store">
+        <button className="agregar-button" onClick={handleAddProduct}>volver</button>
+        <button className="agregar-button" onClick={handleAddProduct}>guardar cambios</button>
+
+    </div>
     </div>
   );
 };
