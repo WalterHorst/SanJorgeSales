9fd58704cc48986c16f67216d9ea78525cafcde8
leonardorodriguez22 <santiyvalenyleo@gmail.com>
2023-12-07 19:37:09 -0300

    asd

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,12 +6,7 @@ import Login from "./views/Login/Login";
 import Footer from "./components/Footer/Footer";
 import About from "./views/About/About";
 import SignUp from "./views/SingUp/SingUp";
-<<<<<<< HEAD
-import AdminPanel from "./components/AdminBoard/AdminBoard"
-import Tienda from "./views/Tienda/Tienda"
-=======
 import Store from "./views/Store/Store";
->>>>>>> 8e44b833431b68654a0726e484e74837b515d9c3
 
 function App() {
   return (
@@ -23,12 +18,7 @@ function App() {
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/about" element={<About></About>}></Route>
           <Route path="/registro" element={<SignUp></SignUp>}></Route>
-<<<<<<< HEAD
-          <Route path="/admin" element={<AdminPanel></AdminPanel>}></Route>
-          <Route path="/tienda" element={<Tienda></Tienda>}></Route>
-=======
           <Route path="/store" element={<Store></Store>}></Route>
->>>>>>> 8e44b833431b68654a0726e484e74837b515d9c3
         </Routes>
         <Footer />
       </div>
